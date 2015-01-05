f891d6b5add7998b82c9464da741cb41e1e7e8be
nbnrain <jcoop@ceh.ac.uk>
2015-01-05 08:56:43 +0000

    Mongodb installation notes

--- mongoTestData.js
+++ mongoTestData.js
@@ -1,6 +1,15 @@
-//A script to populate mongo with geojson points randomly scattered with the bbox of the UK.
-//To use it you need mongo running locally on default port, then open up a mongo console window and run load("mongoTestData.js") - add path as required
-//The UK bbox I've used is: west -9.23, east 2.69, north 60.85, south 49.84
+/*
+A script to populate mongo with geojson points randomly scattered with the bbox of the UK.
+To use it you need mongo running locally on default port, then open up a mongo console window and run load("mongoTestData.js") - add path as required
+The UK bbox I've used is: west -9.23, east 2.69, north 60.85, south 49.84
+
+Mongo installation: http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+  echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+  sudo apt-get update
+  sudo apt-get install -y mongodb-org
+*/
+
 
 function insertData(dbName, collectionName, numPoints){
 	var col = db.getSiblingDB(dbName).getCollection(collectionName);
