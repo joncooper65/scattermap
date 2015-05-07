636915a8cfe96f2826e9a1e860daa018610a806e
Christopher Johnson <cjohn@ceh.ac.uk>
2015-05-07 15:12:37 +0100

    Adding clustering logic

---
+++ import/clusterer.rb
@@ -0,0 +1,26 @@
+class Clusterer
+  def initialize(markers, distance)
+    @markers = markers
+    @distance = distance
+  end
+
+  def cluster_to(clustered)
+    marker = @markers.find_one
+    until marker.nil?
+      query = {"loc" => {"$near" => {"$geometry" => marker['loc'], "$maxDistance" => @distance }}}
+      species = markers.find(query).group_by { |d| d['key'] }.map { |t,docs| {
+        'key' => t,
+        'years' => docs.map{ |d| d['year'] }.uniq,
+        'datasets' => docs.map{ |d| d['dataset'] }.uniq
+      }}
+      clustered.insert([{
+        'position' => marker['loc'],
+        'species'  => species
+      }])
+
+      @markers.remove(query)
+      puts "clustered at #{marker['loc']}"
+      marker = @markers.find_one
+    end
+  end
+end
