041bf4be7ffd2446c26f3736e29da5e59a1f1294
Christopher Johnson <cjohn@ceh.ac.uk>
2015-05-12 11:32:08 +0000

    Removed csv parseing and just using custom tab delimited split code

--- import/darwincore_loader.rb
+++ import/darwincore_loader.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/ruby
 require 'zip'
-require 'csv'
 
 class DarwinCoreArchiveLoader
   def initialize(archive)
@@ -22,12 +21,15 @@ class DarwinCoreArchiveLoader
     bad_records = -> (o) { 
       o.values_at('decimalLongitude', 'decimalLatitude', 'taxonKey', 'year', 'datasetKey').any? { |v| v.nil? }
     }
+    
+    species = -> (o) { o.has_key?('species') }
 
     Zip::File.open(@archive) do |zip_file|
       entry = zip_file.find_entry 'occurrence.txt'
       entry.get_input_stream do |io|
-        csv = CSV.new(io, {:headers => true, :col_sep => "\t"})
-        csv.lazy.reject(&bad_records).map(&gbif_to_mongo).each_slice(1000) {|r| collection.insert r }
+        headers = io.gets.strip.split("\t")
+        tsv = io.lazy.map{ |l| Hash[headers.zip(l.split("\t"))] }
+        tsv.select(&species).reject(&bad_records).map(&gbif_to_mongo).each_slice(1000) {|r| collection.insert r }
       end
     end
   end
