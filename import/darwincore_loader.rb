1a1b53e3b4aed3b86963f1a1e486519ccb89765c
Christopher Johnson <cjohn@ceh.ac.uk>
2015-05-13 10:05:42 +0000

    Adjusting darwincore loader to remove blank entries

--- import/darwincore_loader.rb
+++ import/darwincore_loader.rb
@@ -12,24 +12,22 @@ class DarwinCoreArchiveLoader
     gbif_to_mongo = -> (o) {{
       :loc     => {
         :type => 'Point',
-        :coordinates => [o['decimalLongitude'], o['decimalLatitude'] ] },
+        :coordinates => [o['decimalLongitude'].to_f, o['decimalLatitude'].to_f ] },
       :key     => o['taxonKey'],
       :year    => o['year'],
       :dataset => o['datasetKey']
     }}
 
     bad_records = -> (o) { 
-      o.values_at('decimalLongitude', 'decimalLatitude', 'taxonKey', 'year', 'datasetKey').any? { |v| v.nil? }
+      o.values_at('decimalLongitude', 'decimalLatitude', 'taxonKey', 'year', 'datasetKey', 'species').any? { |v| v.empty? }
     }
     
-    species = -> (o) { o.has_key?('species') }
-
     Zip::File.open(@archive) do |zip_file|
       entry = zip_file.find_entry 'occurrence.txt'
       entry.get_input_stream do |io|
         headers = io.gets.strip.split("\t")
         tsv = io.lazy.map{ |l| Hash[headers.zip(l.split("\t"))] }
-        tsv.select(&species).reject(&bad_records).map(&gbif_to_mongo).each_slice(1000) {|r| collection.insert r }
+        tsv.reject(&bad_records).map(&gbif_to_mongo).each_slice(1000) {|r| collection.insert r }
       end
     end
   end
